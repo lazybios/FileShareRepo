98cbf6b089fbdc33278ce50af5700af5664d20ee
lazybios <lazybios@gmail.com>
2014-07-15 11:00:39 +0800

    add the statsd config file

---
+++ statsd_config.js
@@ -0,0 +1,10 @@
+{
+  graphitePort: 2003
+, graphiteHost: "10.13.81.21"
+, port: 8100
+, mgmt_port: 8100
+, title: "statsd"
+, deleteIdleStats: true
+, prefixStats: "statsd.10_13_81_26.8100"
+, backends: [ "./backends/graphite" ]
+}
